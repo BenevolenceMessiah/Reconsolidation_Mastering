86acad5b6ee825907118147bdbf93e9a06f09012
Michael Pisani <120072200+BenevolenceMessiah@users.noreply.github.com>
2023-11-08 20:26:50 -0500

    Add files via upload
    Added shell/.bat tools

---
+++ Submit_Edits_via_Google_Doc_Windows.bat
@@ -0,0 +1,3 @@
+# Use this to open the Google Doc for collaborative editing. Credit yourself accordingly. Commits/Edits in the Google Doc that are chosen as contributions to the theory will be added to the Main Branch Repo (https://github.com/BenevolenceMessiah/Reconsolidation_Mastering/tree/main).
+
+start https://docs.google.com/document/d/12Ny4bF9UPAJ_Llg3omxbeHFd5NG9yVCDP4GP9y5TLQA/edit?usp=sharing
